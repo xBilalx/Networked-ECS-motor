cbc0763d331228f4bae1b0744ca2125418c90a32
haihuac03 <hai-hua.chen@epitech.eu>
2025-01-29 17:50:53 +0100

    Add: add buttonmodel in menumodel

--- ecs/common/ecs/Model/MenuModel.hpp
+++ ecs/common/ecs/Model/MenuModel.hpp
@@ -11,19 +11,31 @@
 #include <vector>
 #include <functional>
 #include <string>
-
 class MenuModel {
 public:
-    MenuModel(Scene& scene, const std::string& backgroundTexture) {
+    MenuModel(Scene& scene, const std::string& backgroundTexture, std::vector<std::tuple<std::string, float, float>> menuItems, sf::Font& font) {
         menuEntity = scene.createEntity();
-        scene.addComponent<PositionComponent>(menuEntity, 0, 0); // Position du menu (coin supérieur gauche)
-        scene.addComponent<RenderComponent>(menuEntity, backgroundTexture, true); // Chargement de l'image de fond
+        // scene.addComponent<PositionComponent>(menuEntity, 0, 0);
+        // scene.addComponent<RenderComponent>(menuEntity, backgroundTexture, true);
+        
+        float buttonWidth = 200;
+        float buttonHeight = 50;
+
+        for (const auto& [text, x, y] : menuItems) {
+            ButtonModel button(scene, x, y, buttonWidth, buttonHeight, sf::Color::Red, text, font, 24, sf::Color::White);
+            buttonEntities.push_back(button.getEntity()); // Stocker uniquement l'ID des boutons
+        }
     }
 
     std::size_t getEntity() const {
         return menuEntity;
     }
 
+    const std::vector<std::size_t>& getButtons() const {
+        return buttonEntities;
+    }
+
 private:
     std::size_t menuEntity;
+    std::vector<std::size_t> buttonEntities; // Liste des entités des boutons
 };
